e0eabb16803a4ad06bd68a5fb7ee373bcd11ca87
tylerhand1 <tyha7462@colorado.edu>
2024-08-11 21:19:33 -0600

    Add key info to home page

--- src/components/GamePlayer.tsx
+++ src/components/GamePlayer.tsx
@@ -9,8 +9,21 @@ const GamePlayer = () => {
       <div className='screen-container'>
         <Screen />
       </div>
-      <GameUploadForm />
-      <PlayButton />
+      <div className='info-btns-container'>
+        <div className='key-info-container'>
+          <h2>Keys</h2>
+          <p>Chip-8 accepts keys from 0 to 9 and A to F</p>
+          <p>To make this more convenient, they are mapped to be</p>
+          <p>1 2 3 4</p>
+          <p>q w e r</p>
+          <p>a s d f</p>
+          <p>z x c v</p>
+        </div>
+        <div className='btns-container'>
+          <GameUploadForm />
+          <PlayButton />
+        </div>
+      </div>
     </main>
   );
 };
